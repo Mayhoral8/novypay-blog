a6cad497ffab3413fa6311529d201a97dc5eb440
Mayowa Ayeni <mayhoral008@gmail.com>
2025-08-19 08:48:43 +0100

    update project id where necessary

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -7,7 +7,7 @@ import { BlogPost } from "@/lib/sanity/queries";
 
 const BlogPostComp: React.FC<{ post: BlogPost }> = ({ post }) => {
   const builder = imageUrlBuilder({
-    projectId: "lhkm6ysg",
+    projectId: "y4zzsrxn",
     dataset: "production",
   });
   const urlFor = (source: string) => builder.image(source);
@@ -36,13 +36,13 @@ const BlogPostComp: React.FC<{ post: BlogPost }> = ({ post }) => {
           </p>
 
           <div className="flex items-center space-x-4">
-            <Image
+            {/* <Image
                 src={urlFor(post?.heroImage).width(800).height(400).url()}
                 width={800}
                 height={100}
                 alt={post.title}
                 className="w-full h-full object-cover"
-              />
+              /> */}
             <div>
               <div className="flex items-center space-x-2">
                 <span className="font-semibold text-gray-200">
